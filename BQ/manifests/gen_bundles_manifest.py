e68d95e2cc04a244999ac882082a76126ff68472
BillClifford <bcli4d>
2020-11-03 14:22:36 -0800

    Only include series and studies with fewer than some number of instances.

--- BQ/manifests/gen_bundles_manifest.py
+++ BQ/manifests/gen_bundles_manifest.py
@@ -14,11 +14,28 @@ def gen_bundles_manifest(args):
 
     query_job = client.query(query)  # Make an API request.
 
+    series_lengths = {}
+
     with open(args.csv_file, 'w') as f:
         print("bundle_names, ids", file=f )
         for row in query_job:
-            # Row values can be accessed by field name or index.
-            print("{},{}".format(json.dumps(row['bundle_names']).replace('"',''),json.dumps(row['ids']).replace('"','')), file=f)
+            if row['ids'][0].startswith('dg'):
+                # It's a series
+                series_lengths[row['bundle_names']] = len(row['ids'])
+                if len(row['ids']) <= args.max_instances:
+                    # Index this series
+                    print("{},{}".format(json.dumps(row['bundle_names']).replace('"',''),json.dumps(row['ids']).replace('"','')), file=f)
+                else:
+                    print("Series {} too long: {}".format(row['bundle_names'], len(row['ids'])))
+            else:
+                # It's a study
+                study_length = sum([series_lengths[series] for series in row['ids']])
+                if study_length <= args.max_instances:
+                    # Index this study
+                    print("{},{}".format(json.dumps(row['bundle_names']).replace('"',''),json.dumps(row['ids']).replace('"','')), file=f)
+                else:
+                    print("Study {} too long: {}".format(row['bundle_names'], study_length))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -28,6 +45,7 @@ if __name__ == '__main__':
                         help="CSV file in which to save results")
     parser.add_argument('--collections', default='all'.format(os.environ['PWD']),
                         help="File containing list of IDC collection IDs or 'all' for all collections")
+    parser.add_argument('--max_instances', default=3000, type=int, help="Maximum number of instances in a series or study")
     parser.add_argument('--project', default="idc-dev-etl")
     args = parser.parse_args()
     print("{}".format(args), file=sys.stdout)
