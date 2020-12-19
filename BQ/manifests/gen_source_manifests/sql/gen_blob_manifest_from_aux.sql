aae3866d34a234c4dd577671b33cebcfeb5609a4
BillClifford <bcli4d>
2020-12-19 15:20:08 -0800

    Convert md5 hashes to hex when generating manifest for DCF.

--- BQ/manifests/gen_source_manifests/sql/gen_blob_manifest_from_aux.sql
+++ BQ/manifests/gen_source_manifests/sql/gen_blob_manifest_from_aux.sql
@@ -1,3 +1,3 @@
-select CRDC_UUIDS.instance as GUID, MD5_Hash as md5, Instance_size as size, "*" as acl, GCS_URL as url
+select CRDC_UUIDS.instance as GUID, TO_HEX(FROM_BASE64(MD5_Hash)) as md5, Instance_size as size, "*" as acl, GCS_URL as url
 from {aux}
 where IDC_Webapp_CollectionID = "{collection}"
